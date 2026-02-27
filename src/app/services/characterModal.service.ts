934f1d9c1bf82e510d375e093026a4bf2cf365ae
Augustine <a.uzochukwu@vanlanschotkempen.com>
2026-02-27 16:28:20 +0100

    last commit

--- src/app/services/characterModal.service.ts
+++ src/app/services/characterModal.service.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from "@angular/common";
 import { Component, computed, effect, HostListener, inject, Injectable, signal } from "@angular/core";
 import { CharacterService, Character } from "../services/character.service";
 import { EpisodeService, Episode } from "./episode.service";
+import { switchMap, tap, of } from "rxjs";
 
 
 @Injectable({
@@ -22,20 +22,21 @@ export class CharacterModalService {
         this.character.set(null);
         this.episodes.set([]);
         
-        this.characterService.getCharacter(characterId).subscribe(character => {
-            this.character.set(character);
-            
-            // Fetch episode details
-            if (character.episode && character.episode.length > 0) {
-                const episodeIds = character.episode
-                    .map(url => url.split('/').pop())
-                    .join(',');
-                
-                this.episodeService.getMultipleEpisodes(episodeIds).subscribe(episodes => {
-                    // Handle both single episode (object) and multiple episodes (array)
-                    this.episodes.set(Array.isArray(episodes) ? episodes : [episodes]);
-                });
-            }
+        this.characterService.getCharacter(characterId).pipe(
+            tap(character => this.character.set(character)),
+            switchMap(character => {
+                // Fetch episode details if episodes exist
+                if (character.episode && character.episode.length > 0) {
+                    const episodeIds = character.episode
+                        .map(url => url.split('/').pop())
+                        .join(',');
+                    
+                    return this.episodeService.getMultipleEpisodes(episodeIds);
+                }
+                return of([]); // Return empty array if no episodes
+            })
+        ).subscribe(episodes => {
+            this.episodes.set(Array.isArray(episodes) ? episodes : [episodes]);
         });
     }
 
